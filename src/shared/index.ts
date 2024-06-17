661e3e49dc5c99ae4db5e894c1469ac11d7e402d
Valmir Thume Junior <juninhothume21@gmail.com>
2024-06-17 19:17:42 -0300

    test: Adiciona a configuração do Jest ao projeto

---
+++ src/shared/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: 'Start Route with TS-NODE-DEV'
+  });
+});
+
+app.listen(process.env.APP_PORT);
